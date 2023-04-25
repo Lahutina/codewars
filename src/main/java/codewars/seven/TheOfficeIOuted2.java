29de466953d6cdd63461a0385c9a2267a535cddf
Oksana Lahutina <oksankalahutina@gmail.com>
2023-04-25 22:48:45 +0300

    Update The Office I - Outed

--- src/main/java/codewars/seven/TheOfficeIOuted2.java
+++ src/main/java/codewars/seven/TheOfficeIOuted2.java
@@ -7,11 +7,11 @@ https://www.codewars.com/kata/57ecf6efc7fe13eb070000e1/train/java
 public class TheOfficeIOuted2 {
     public static String outed(Person[] meet, String boss) {
         double totalScore = 0.0;
-        int i = 0;
 
-        for( ; i < meet.length; i++)
-            totalScore +=  meet[i].name.equals(boss) ? 2 * meet[i].happiness : meet[i].happiness;
+        for (Person person : meet) {
+            totalScore += person.name.equals(boss) ? 2 * person.happiness : person.happiness;
+        }
 
-        return totalScore/i <= 5.0 ? "Get Out Now!" : "Nice Work Champ!";
+        return totalScore / meet.length <= 5.0 ? "Get Out Now!" : "Nice Work Champ!";
     }
 }
