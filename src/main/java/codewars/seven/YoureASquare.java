3ecc31ef2e9f4a3acc3262bfe8e2e4ca9fb6d0bd
Oksana Lahutina <oksankalahutina@gmail.com>
2023-05-01 15:12:30 +0300

    Remove unnecessary check

--- src/main/java/codewars/seven/YoureASquare.java
+++ src/main/java/codewars/seven/YoureASquare.java
@@ -6,6 +6,6 @@ https://www.codewars.com/kata/54c27a33fb7da0db0100040e/train/java
 
 public class YoureASquare {
     public static boolean isSquare(int n) {
-        return Math.sqrt(n) % 1 == 0 && n >= 0;
+        return Math.sqrt(n) % 1 == 0;
     }
 }
