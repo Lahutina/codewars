042b75fd9dd16b28dec1b53c38867f8cfbbc2f71
Oksana Lahutina <oksankalahutina@gmail.com>
2023-05-10 11:15:24 +0300

    Persistent Bugger implementation

---
+++ src/test/java/codewars/six/PersistentBuggerTest.java
@@ -0,0 +1,16 @@
+package codewars.six;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PersistentBuggerTest {
+
+    @Test
+    public void basicTests() {
+        assertEquals(3, PersistentBugger.persistence( 39), "Incorrect answer for n=39" );
+        assertEquals(0, PersistentBugger.persistence(  4), "Incorrect answer for n=4"  );
+        assertEquals(2, PersistentBugger.persistence( 25), "Incorrect answer for n=25" );
+        assertEquals(4, PersistentBugger.persistence(999), "Incorrect answer for n=999");
+    }
+}
